898b50108cfa74a455069268cc304ce2c6cc9169
Branquinho <38164699+BranquinhodSouza@users.noreply.github.com>
2018-07-24 12:09:50 -0300

    Add files via upload

--- WebContent/area-restrita-admin/cadastrar-servico.java
+++ WebContent/area-restrita-admin/cadastrar-servico.java
@@ -5,36 +5,40 @@
 
   <div class="container">
     <div class="section-header">
-      <h2>Cadastrar Serviços</h2>
+      <h2>Alterar Informações</h2>
     </div>
   
     <div class="form">
       <form action="servico" method="post">
 
         <div class="form-row">
+          <input type="number" name="id" value="" style="display: none;">
+          
           <div class="form-group col-md-6">
             <label>Descrição</label>
-            <input type="text" name="descrição" class="form-control" value=""
+            <input type="text" name="descricao" class="form-control" value=""
               placeholder="Digite a descrição" required="required">
           </div>
-        </div>
-        
-        <div class="form-row">
-          <div class="form-group col-md-6">
-          <label>Características</label>
-            <input type="text" class="form-control" name="caracteristica" value=""
-            placeholder="Digite as caracteristicas" required="required">
-          </div>
-        </div>
-        
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label>Valor</label>
-            <input type="number" class="form-control" name="valor" value=""
-              placeholder="Digite o valor" >
-          </div>
           
+          <div class="form-group col-md-6">
+            <label>Caracteristica</label>
+            <input type="text" class="form-control" name="características" value=""
+               placeholder="Digite as características" required="required">
+          </div>
         </div>
+        
+        <div class="form-row">
+          <div class="form-group col-md-4">
+            <label>Valores</label>
+            <input type="number" class="form-control" name="valorpagina" value=""
+            placeholder="Digite o valor por página" maxlength="14">
+
+            <input type="number" class="form-control" name="valorlauda" value=""
+            placeholder="Digite o valor por lauda" maxlength="14">
+            
+            <input type="number" class="form-control" name="valorpalavra" value=""
+            placeholder="Digite o valor por character" maxlength="14">
+          </div>
         
         <div class="form-group float-right">
           <button type="submit" class="btn btn-default">Salvar</button>
