261d6ba8f8798989e461173cbb539c91e9e0ca5c
lucasasafe123 <lucasasafe_@hotmail.com>
2018-07-25 16:41:29 -0300

    Utils "comentado"

--- src/br/edu/ifg/util/Utils.java
+++ src/br/edu/ifg/util/Utils.java
@@ -5,9 +5,10 @@ import java.util.Locale;
 
 public class Utils {
 	
+	//Metodo usado para que o valor no input seja formatado adequadamente
 	public static String formatarValor(Double valor) {
-		NumberFormat fmt = NumberFormat.getInstance(new Locale("pt", "BR"));
-		fmt.setMinimumFractionDigits(2);
-		return fmt.format(valor);
+		NumberFormat fmt = NumberFormat.getInstance(new Locale("pt", "BR"));//Localização
+		fmt.setMinimumFractionDigits(2);//Numeros de casas apos a virgula
+		return fmt.format(valor);// Retorna formatado
 	}
 }
