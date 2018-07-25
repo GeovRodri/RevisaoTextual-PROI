654c995fb306f1f5deb900b0c16b707115b184db
lucasasafe123 <lucasasafe_@hotmail.com>
2018-07-25 16:22:36 -0300

    Corrigindo erros de compilação

--- src/br/edu/ifg/servlet/arearestritaadmin/ListarServicoServlet.java
+++ src/br/edu/ifg/servlet/arearestritaadmin/ListarServicoServlet.java
@@ -1,16 +1,12 @@
 package br.edu.ifg.servlet.arearestritaadmin;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import br.edu.ifg.dao.ServicoDAO;
-import br.edu.ifg.model.Servico;
 
 @WebServlet("/area-restrita-admin/listar-servicos")
 public class ListarServicoServlet extends HttpServlet {
@@ -18,18 +14,18 @@ public class ListarServicoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected List<Servico> doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//Retornando uma lista de servicos
 		ServicoDAO servicoDAO = new ServicoDAO();
-		List<Servico> servicos = servicoDAO.getAll();
-		return servicos; 
+		servicoDAO.getAll();
+		 
 	}
 
 
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		//Removendo servico
 		ServicoDAO servicoDAO = new ServicoDAO();
-		servicoDAO.excluirServico(request.getAttribute("id"));
+		servicoDAO.excluirServico((Integer) request.getAttribute("id"));
 	}
 
 
