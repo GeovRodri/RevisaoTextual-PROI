41cccc757be0206a1381be7ec44c509a99212ae9
Branquinho <38164699+BranquinhodSouza@users.noreply.github.com>
2018-07-25 17:35:01 -0300

    adicionando comentário

--- src/br/edu/ifg/jdbc/ConnectionFactory.java
+++ src/br/edu/ifg/jdbc/ConnectionFactory.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/* Classe responsável por criar a conexão com a base de dados
+*/
 public class ConnectionFactory {
 
 	public Connection getConnection() {
